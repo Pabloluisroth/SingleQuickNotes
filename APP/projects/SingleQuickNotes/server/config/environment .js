b1f5550480c48ec9a96313409cd193483bef4ccc
SingleQuickNotes <95092551+Pabloluisroth@users.noreply.github.com>
2023-06-12 22:48:42 +0200

    act

--- APP/projects/SingleQuickNotes/server/config/environment .js
+++ APP/projects/SingleQuickNotes/server/config/environment .js
@@ -3,6 +3,7 @@
 module.exports = {
   port: process.env.PORT_API || 3000,
   log:process.env.LOG || 'SingleQuickNotes\server\logs\server.log',
+  // db:process.env.DATABASE_URL || 'mongodb+srv://admin:SingleQuickNotes@cluster0.v5hhzmy.mongodb.net/tareas'
 };
 
 
